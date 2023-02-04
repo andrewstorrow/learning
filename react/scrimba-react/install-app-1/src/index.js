1f3b21d9cf202bae83f871f891d34fef928d02b4
Andrew Storrow <andrew.storrow@gmail.com>
2023-02-04 15:52:58 -0800

    implemented basic react page

--- react/scrimba-react/install-app-1/src/index.js
+++ react/scrimba-react/install-app-1/src/index.js
@@ -3,13 +3,58 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import logo from './logo.svg';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+function Header() {
+  return (
+    <header>
+      <nav className="nav">
+        <img src={logo} className="nav-logo" />
+        <ul className="nav-items">
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+function MainContent() {
+  return (
+    <div className="main-content">
+      <h1>Reasons I'm excited to learn React</h1>
+      <ol>
+        <li>It's a popular library, so I'll be
+          able to fit in with the cool kids!</li>
+        <li>I'm more likely to get a job as a developer
+          if I know React</li>
+      </ol>
+    </div>
+  )
+}
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <small>© 2023 Storrow development. All rights reserved.</small>
+    </footer>
+  )
+}
+
+function Page() {
+  return (
+    <>
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
+  )
+}
+
+root.render(<Page />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
