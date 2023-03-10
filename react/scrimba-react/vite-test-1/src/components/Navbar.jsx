5574641039ecc55e62eeef1c6aab0f3ae907e7e5
Andrew Storrow <andrew.storrow@gmail.com>
2023-03-09 17:05:15 -0800

    implemented navbar

--- react/scrimba-react/vite-test-1/src/components/Navbar.jsx
+++ react/scrimba-react/vite-test-1/src/components/Navbar.jsx
@@ -3,7 +3,9 @@ import reactLogo from '../images/react-icon-small.png';
 function Navbar() {
     return (
         <nav>
-            <img src={reactLogo} alt="" />
+            <img src={reactLogo} alt="react logo" />
+            <h3>ReactFacts</h3>
+            <h4>React Course - Project 1</h4>
         </nav>
     )
 }
