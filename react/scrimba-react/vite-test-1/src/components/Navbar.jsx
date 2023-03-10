acbadb5c078c69314c5f02e8bd0172185eec633f
Andrew Storrow <andrew.storrow@gmail.com>
2023-03-09 16:39:36 -0800

    setting up vite react projects

---
+++ react/scrimba-react/vite-test-1/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import reactLogo from '../images/react-icon-small.png';
+
+function Navbar() {
+    return (
+        <nav>
+            <img src={reactLogo} alt="" />
+        </nav>
+    )
+}
+
+export default Navbar
